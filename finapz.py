f72fbc3612855d6566ba407f5c652a557d649f1e
Vlado <vgrozev@gmail.com>
2020-01-11 15:21:31 -0500

    first commit

--- finapz.py
+++ finapz.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "<h1>Deployed to Heroku!</h1>"
+
+
+@app.route('/vlado')
+def index():
+    return "<h1>Vlado's space</h1>"
