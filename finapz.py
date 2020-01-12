62683f49cb477f73c6e16ed7e1e0416538504403
lranchev <lranchev@gmail.com>
2020-01-11 23:50:22 -0500

    first API call

--- finapz.py
+++ finapz.py
@@ -27,3 +27,7 @@ def lacho():
     ts = TimeSeries(key='FH0XYNHWNTKNS1PG')
     data, meta_data = ts.get_daily('AKAM')
     return "Lacho's space. \n\n Trying to display some data for AKAM stock here:\n\n" + str(data)
+
+
+if __name__ == '__main__':
+ app.run()
