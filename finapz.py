80bfaa9cca0a5afa3de90cc48f49ed3a74db37fc
lranchev <lranchev@gmail.com>
2020-01-12 18:11:12 -0500

    first API call

--- finapz.py
+++ finapz.py
@@ -1,32 +1,35 @@
-from flask import Flask
-from alpha_vantage.timeseries import TimeSeries
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return "<h1>Deployed to Heroku!</h1>"
-
-
-@app.route('/vlado')
-def vlado_tmp():
-    return "<h1>Vlado's space</h1>"
-
-
-# Plamen's route, yeah babe!!!
-@app.route('/paxi')
-def paxi_tmp():
-    return "<h1>Paxi's space</h1>"
-
-
-# Plamen's route, yeah babe!!!
-@app.route('/lacho')
-def lacho():
-    ts = TimeSeries(key='FH0XYNHWNTKNS1PG')
-    data, meta_data = ts.get_daily('AKAM')
-    return "Lacho's space. \n\n Trying to display some data for AKAM stock here:\n\n" + str(data)
+from templates import app
+# from flask import Flask
+# from alpha_vantage.timeseries import TimeSeries
+#
+# app = Flask(__name__,
+#             static_folder = './public',
+#             template_folder="./static"
+#             )
+#
+#
+# @app.route('/')
+# def index():
+#     return "<h1>Deployed to Heroku!</h1>"
+#
+#
+# @app.route('/vlado')
+# def vlado_tmp():
+#     return "<h1>Vlado's space</h1>"
+#
+#
+# # Plamen's route, yeah babe!!!
+# @app.route('/paxi')
+# def paxi_tmp():
+#     return "<h1>Paxi's space</h1>"
+#
+#
+# # Plamen's route, yeah babe!!!
+# @app.route('/lacho')
+# def lacho():
+#     ts = TimeSeries(key='FH0XYNHWNTKNS1PG')
+#     data, meta_data = ts.get_daily('AKAM')
+#     return "Lacho's space. \n\n Trying to display some data for AKAM stock here:\n\n" + str(data)
 
 
 if __name__ == '__main__':
--- templates/__init__.py
+++ templates/__init__.py
@@ -1,8 +1,36 @@
-from flask import Flask
-app = Flask(__name__,
- 	static_folder = './public',
- 	template_folder="./static")
 
-from templates.hello.views import hello_blueprint
-# register the blueprints
-app.register_blueprint(hello_blueprint)
+from flask import Flask
+from alpha_vantage.timeseries import TimeSeries
+
+app = Flask(__name__,
+			static_folder = './public',
+			template_folder="./static"
+			)
+
+
+@app.route('/')
+def index():
+	return "<h1>Deployed to Heroku!</h1>"
+
+
+@app.route('/vlado')
+def vlado_tmp():
+	return "<h1>Vlado's space</h1>"
+
+
+# Plamen's route, yeah babe!!!
+@app.route('/paxi')
+def paxi_tmp():
+	return "<h1>Paxi's space</h1>"
+
+
+@app.route('/lacho')
+def lacho():
+	ts = TimeSeries(key='FH0XYNHWNTKNS1PG')
+	data, meta_data = ts.get_daily('AKAM')
+	return "Lacho's space. \n\n Trying to display some data for AKAM stock here:\n\n" + str(data)
+
+#
+# from templates.hello.views import hello_blueprint
+# # register the blueprints
+# app.register_blueprint(hello_blueprint)
